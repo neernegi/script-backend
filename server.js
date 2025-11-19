2f0be31f169557a22dde27a8140228451580b912
Neeraj <neerajnegi3443@gmail.com>
2025-11-19 17:18:28 +0530

    dd

--- server.js
+++ server.js
@@ -10,22 +10,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://script-backend-eq54.onrender.com",
-    credentials: true,
-  })
-);
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import taskRouter from "./routes/task.route.js";
-import boardRouter from "./routes/board.route.js";
 
 dotenv.config();
 
-const app = express();
 
 app.use(express.json());
 app.use(
@@ -45,12 +32,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-connectDB();
-
-app.use("/", taskRouter);
-app.use("/", boardRouter);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
