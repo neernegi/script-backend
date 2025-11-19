4f89987b09e0b1dc896125ad4177a4736ac8aae4
Neeraj <neerajnegi3443@gmail.com>
2025-11-19 17:10:43 +0530

    vv

--- server.js
+++ server.js
@@ -16,6 +16,34 @@ app.use(
     credentials: true,
   })
 );
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import taskRouter from "./routes/task.route.js";
+import boardRouter from "./routes/board.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+connectDB();
+
+app.use("/", taskRouter);
+app.use("/", boardRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 connectDB();
 
